e2cf5599c56627c503055435bf0ad6751756e62e
Fredrik Andersson <freand01@gafe.molndal.se>
2025-12-05 14:49:21 +0100

    Fix: Downgraded to stable Tailwind v3

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 export default {
   plugins: {
-    // VIKTIG KORRIGERING: Använder nu det moderna sättet att inkludera Tailwind
-    'tailwindcss/nesting': {}, 
     tailwindcss: {},
     autoprefixer: {},
   },
